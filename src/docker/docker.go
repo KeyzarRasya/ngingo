8ea713bca0f96c2026af52769a9a6c7a649f308a
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-07-06 20:12:56 +0700

    refactoring: adding interface to make it testable

--- src/docker/docker.go
+++ src/docker/docker.go
@@ -4,48 +4,67 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"net/http"
 
-	"github.com/KeyzarRasya/ngingo/src/cpu"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
-func Init() (*client.Client, error) {
-	return client.NewClientWithOpts(
-			client.WithHost("unix:///var/run/docker.sock"),
-			client.WithVersion("1.50"),
-	)
+type Service interface {
+	AddContainer()	error;
+	ReadStat()	(map[uint16]*Stat, error);
 }
 
-func ReadStat(client *client.Client, ctr container.Summary) (*cpu.Stat, error) {
-	resp, err := client.ContainerStats(context.Background(), ctr.ID, true)
+type DockerService struct {
+	dockerInstance		*client.Client
+}
+
+func NewDockerService() (*DockerService, error) {
+	client, err :=  client.NewClientWithOpts(
+		client.WithHost("unix:///var/run/docker.sock"),
+		client.WithVersion("1.50"),
+	)
 
 	if err != nil {
-		fmt.Printf("Failed to read container stats : %s", err.Error());
+		return nil, err
+	}
+
+	return &DockerService{dockerInstance: client}, nil
+}
+
+func (ds *DockerService) AddContainer() error {
+	return nil
+}
+
+func (ds *DockerService) ReadStat() (map[uint16]*Stat, error) {
+	containers, err := ds.dockerInstance.ContainerList(context.Background(), container.ListOptions{})
+
+	var portStat map[uint16]*Stat = make(map[uint16]*Stat);
+
+	portChan := make(chan uint16, len(containers))
+	statChan := make(chan *Stat, len(containers)) 
+
+	if err != nil {
 		return nil, err;
 	}
 
-	defer resp.Body.Close()
+	for _, ctr := range containers {
+		go ds.readStatCh(ctr, portChan, statChan)
+	} 
 
-	
-	var stat cpu.Stat;
+	for i := 0; i < len(containers); i++ {
 
-	decoder := json.NewDecoder(resp.Body) 
-	err = decoder.Decode(&stat)
-	
-	if err != nil {
-		fmt.Println(err.Error())
-		return nil, err
+		stat := <-statChan
+		port := <- portChan
+
+		portStat[port] = stat;
+
 	}
-	//fmt.Printf("[%s] Name : %s  | CPU Percentage : %f | Read : %s\n", state, stat.Name, stat.CpuPercentage, stat.ReadTime.String())
 
-	return &stat, nil;
-
+	return portStat, nil
 }
 
-func ReadStatCh(client *client.Client, ctr container.Summary, port chan<- uint16, cpuStat chan<- *cpu.Stat) error {
-	resp, err := client.ContainerStatsOneShot(context.Background(), ctr.ID)
+func (ds *DockerService) readStatCh(ctr container.Summary, port chan<- uint16, cpuStat chan<- *Stat) error {
+	resp, err := ds.dockerInstance.ContainerStatsOneShot(context.Background(), ctr.ID)
 
 	if err != nil {
 		fmt.Printf("Failed to read container stats : %s", err.Error());
@@ -56,7 +75,7 @@ func ReadStatCh(client *client.Client, ctr container.Summary, port chan<- uint16
 
 	defer resp.Body.Close()
 
-	var stat cpu.Stat;
+	var stat Stat;
 
 	decoder := json.NewDecoder(resp.Body) 
 	err = decoder.Decode(&stat)
@@ -73,44 +92,4 @@ func ReadStatCh(client *client.Client, ctr container.Summary, port chan<- uint16
 	return nil
 }
 
-func ReadCPUUsagePerRequest(client *client.Client, ctr container.Summary) error {
-	before,  err := ReadStat(client, ctr)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return err;
-	}
-
-	for range 3000 {
-		_, err = http.Get(fmt.Sprintf("http://localhost:%d/", ctr.Ports[1].PublicPort))
-	
-		if err != nil {
-			fmt.Println(err.Error())
-			return err;
-		}
-	}
-
-	after, err := ReadStat(client, ctr);
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return err;
-	}
-
-	cpuPercent := ReadCPUUsage(before, after);
-
-	fmt.Printf("[%d] CPU used by request: %.6f%%\n", ctr.Ports[1].PublicPort, cpuPercent)
-
-	return nil
-}
-
-func ReadCPUUsage(before, after *cpu.Stat) float64 {
-	cpuDelta := after.CpuStat.CpuUsage.TotalUsage - before.CpuStat.CpuUsage.TotalUsage
-	sysDelta := after.CpuStat.SysCpuUsage - before.CpuStat.SysCpuUsage
-	onlineCPU := after.CpuStat.OnlineCpu
-
-	cpuPercent := (float64(cpuDelta) / float64(sysDelta)) * float64(onlineCPU) * 100
-
-	return cpuPercent
-}
 
