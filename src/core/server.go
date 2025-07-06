e559abe7ead1c4cc245b25cd4248eff14c189947
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-07-06 20:11:27 +0700

    refactor: refactoring server Run function

--- src/core/server.go
+++ src/core/server.go
@@ -1,144 +1,110 @@
 package core
 
 import (
-	"context"
-	"errors"
 	"fmt"
-	"net/http"
-	"sort"
-	"time"
+	"io"
 
-	"github.com/KeyzarRasya/ngingo/src/cpu"
+	"net/http"
+	"net/url"
+
+	"github.com/KeyzarRasya/ngingo/src/balancer"
 	"github.com/KeyzarRasya/ngingo/src/docker"
+	"github.com/KeyzarRasya/ngingo/src/files"
 	"github.com/KeyzarRasya/ngingo/src/model"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
 )
 
 type Server struct {
 	NgingoConfiguration NgingoConfiguration;
-	DockerClient		*client.Client;
+	Service 			*docker.DockerService
+	HttpClient			*http.Client;
+	Balancer			balancer.Balancer;
+	DataFiles			files.DataWriteRead
 }
-
-type PortCPU struct {
-	Port 	uint16;
-	CPU	float64;
-}
-
 
 func (s *Server) Run(config model.Configuration) {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World | %s", r.URL)
+		var records [][]string;
+		pcpu, err := s.Balancer.LowestUsage()
+		if err != nil {
+			return;
+		}
+
+		before, err := s.Balancer.Usages();
+		if err != nil {
+			fmt.Println(err.Error())
+			return;
+		}
+		
+		b, err := s.processRequest(r, pcpu)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		
+		pcpu.Endpoint = r.URL.Path
+
+		after, err := s.Balancer.Usages()
+		if err != nil {
+			fmt.Println(err.Error())
+			return;
+		}
+
+		diff := (after[pcpu.Port] - before[pcpu.Port])
+
+		record := []string{fmt.Sprintf("%d", pcpu.Port), pcpu.Endpoint, fmt.Sprintf("%f", diff)}
+		records = append(records, record)
+
+		s.DataFiles.Write(records)
+
+		fmt.Printf("===\n[PORT : %d]\n Before Usage : %f\n After Usage :  %f\n Diff : %f\n===\n", pcpu.Port, before[pcpu.Port], after[pcpu.Port], diff)
+
+	
+
+		fmt.Fprint(w, string(b))
+
 	})
 
-	pcpu, err := s.HighestCPU()
+	fmt.Printf("Running at Port %s", config.Port)
 
-	if err != nil {
-		return
-	}
-
-	fmt.Printf("Highest is PORT %d with Usage of : %f \n", pcpu.Port, pcpu.CPU)
 	http.ListenAndServe(fmt.Sprintf(":%s", config.Port), nil)
 
-	fmt.Printf("Running at Port %s", config.Port)
 
 	for {}
 }
 
-func (s *Server) HighestCPU() (*PortCPU, error) {
-	start := time.Now()
-	var sortedPortCpus []PortCPU;
-	portCpus, err := s.portAndCPU()
+
+func (s *Server) processRequest(r *http.Request, portCpu *balancer.EndpointCPUStat) ([]byte, error) {
+	url, err := url.Parse(r.URL.String())
 
 	if err != nil {
 		return nil, err
 	}
 
-	for port, cpu := range portCpus {
-		var portCpu PortCPU = PortCPU{
-			Port: port,
-			CPU: cpu,
-		}
-		sortedPortCpus = append(sortedPortCpus, portCpu)	
-	}
+	endpoint := fmt.Sprintf("http://localhost:%d%s", portCpu.Port, url.Path)
 
-	sortPortCPU(&sortedPortCpus)
-
-	duration := time.Since(start)
-	fmt.Printf("Reading took %s\n", duration)
-
-	return &sortedPortCpus[0], nil
-
-}
-
-func sortPortCPU(portCPU *[]PortCPU) error {
-	sort.Slice(*portCPU, func(i, j int) bool {
-		return (*portCPU)[i].CPU > (*portCPU)[j].CPU 
-	})
-	return nil
-}
-
-
-func (s *Server) portAndCPU() (map[uint16]float64, error) {
-	var portCpu map[uint16]float64 = make(map[uint16]float64)
-	before, err := s.portAndStat()
+	req, err := http.NewRequest(r.Method, endpoint, nil);
 
 	if err != nil {
 		return nil, err
 	}
 
-	time.Sleep(2 * time.Millisecond)
+	req.Header = r.Header.Clone()
 
-	after, err := s.portAndStat()
-
-	if err != nil {
-		return nil, err
-	}
-
-	if len(before) != len(after) {
-		return nil, errors.New("There is might be one Port Down")
-	}
-
-	for port, stat := range before {
-		portCpu[port] = docker.ReadCPUUsage(stat, after[port])
-	}
-
-	fmt.Printf("%f\n",portCpu[3000])
-	fmt.Printf("%f\n",portCpu[3001])
-	fmt.Printf("%f\n",portCpu[3002])
-
-	return portCpu, nil
-
-	
-}
-
-func (s *Server) portAndStat() (map[uint16]*cpu.Stat, error) {
-	containers, err := s.DockerClient.ContainerList(context.Background(), container.ListOptions{})
-	var portStat map[uint16]*cpu.Stat = make(map[uint16]*cpu.Stat);
-
-
-	portCh := make(chan uint16, len(containers))
-	statCh := make(chan *cpu.Stat, len(containers))
-
+	res, err := s.HttpClient.Do(req)
 
 	if err != nil {
 		return nil, err;
 	}
 
-	for _, ctr := range containers {
-		go docker.ReadStatCh(s.DockerClient, ctr, portCh, statCh)
+	defer res.Body.Close()
+
+	b, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
 	}
 
-	for i := 0; i < len(containers); i++ {
-
-		stat := <-statCh
-		port := <- portCh
-
-		portStat[port] = stat;
-
-	}
-
-	return portStat, nil
-
+	return b, nil;
+	
 }
